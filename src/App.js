ddd64254ddbcbf929a572d4e5859e5ca53e7c4f7
Valeri <valeri.t.yanev@gmail.com>
2023-01-12 11:18:51 +0200

    dynamically outputting expenses by sorting them

--- src/App.js
+++ src/App.js
@@ -1,36 +1,40 @@
+import { useState } from "react";
 import Expenses from "./Components/Expenses/Expenses";
 import NewExpense from "./Components/NewExpense/NewExpense";
 
-const App = () => {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Car Insurance",
-      amount: 239,
-      date: new Date(2023, 0, 5),
-    },
-    {
-      id: "e2",
-      title: "New Desk",
-      amount: 300,
-      date: new Date(2021, 3, 5),
-    },
-    {
-      id: "e3",
-      title: "New Bed",
-      amount: 405,
-      date: new Date(2022, 5, 21),
-    },
-    {
-      id: "e4",
-      title: "Smartphone",
-      amount: 1010,
-      date: new Date(2021, 0, 4),
-    },
-  ];
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Car Insurance",
+    amount: 239,
+    date: new Date(2023, 0, 5),
+  },
+  {
+    id: "e2",
+    title: "New Desk",
+    amount: 300,
+    date: new Date(2021, 3, 5),
+  },
+  {
+    id: "e3",
+    title: "New Bed",
+    amount: 405,
+    date: new Date(2022, 5, 21),
+  },
+  {
+    id: "e4",
+    title: "Smartphone",
+    amount: 1010,
+    date: new Date(2021, 0, 4),
+  },
+];
 
+const App = () => {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
   const addExpenseHandler = (expense) => {
-    console.log(expense);
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
   };
 
   return (
